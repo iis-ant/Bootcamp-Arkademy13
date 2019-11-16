8da81398ed999b2dcdc8c6b4877a00c0f0fdf641
iis-ant <55619460+iis-ant@users.noreply.github.com>
2019-11-16 21:51:18 +0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,16 +1,25 @@
-function printWord(data) {
-	var voc = "";
-	var res = voc.toLowerCase();
-	var kons = "";
-
-	for (var i = 0; i < data.length; i++) {
-		if (data[i] == 'A' || data[i] == 'I' || data[i] == 'U' || data[i] == 'E' || data[i] == 'O') {
-			res = res + data[i] + '\n'
-		} else {
-			kons = kons + data[i] + '\n'
-		}
-	}
-	console.log(res+kons);
+function findPair(arr) {
+  if (!Array.isArray(arr)) return "Bukan array!";
+  else {
+    var num = new Array(101);
+    var jumlah = 0;
+    for (var i = 0; i < 101; i++) {
+      num[i] = 0;
+    }
+    for (var i = 0; i < arr.length; i++) {
+      num[arr[i]]++;
+    }
+    for (var i = 0; i < num.length; i++) {
+      if (num[i] > 1) {
+        jumlah += parseInt(num[i] / 2);
+      }
+    }
+    if (jumlah === 0) return "Tidak ada pasangan yang ditemukan";
+    else return `${jumlah} pasang`;
+  }
 }
 
-printWord("ARKADEMY")
+console.log(findPair([1, 5, 5, 10, 9, 13, 5, 1]));
+console.log(findPair([20, 50, 70, 10, 70, 30, 10, 30, 50]));
+console.log(findPair([1, 2, 3, 4, 5]));
+console.log(findPair("aku ganteng"));
