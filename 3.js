7ae16bcc945b8750712f5e92289e3a10d0d14be8
iis-ant <55619460+iis-ant@users.noreply.github.com>
2019-11-16 21:49:42 +0700

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,18 +1,11 @@
-function thirdHighest(data){
-	if (Array.isArray(data) == true) {
-		if (data.length > 2) {
-			data.sort((a, b) => {
-				return b-a
-			});
-
-			return data[2]
-		} else {
-			return "Minimal array length is 3!";
-		}
-	} else {
-		return "Parameter should be an array!";
-	}
+function cek_kata(kata) {
+  var daftarKata = kata.split(" ");
+  var jumlah = 0;
+  for (var i=0 ; i<daftarKata.length ; i++) {
+    if (isNaN(Number(daftarKata[i]))) jumlah++;
+  }
+  console.log(`${jumlah}/${daftarKata.length}`);
 }
 
-var data = [1,2,3,4,5,6];
-console.log(thirdHighest(data));
+cek_kata("ini adalah sebuah kata");
+cek_kata("2 pasang sandal hilang");
