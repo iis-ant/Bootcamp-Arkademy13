4650c6f9ee3fb301212bb3e273641cbcc95e6729
iis-ant <55619460+iis-ant@users.noreply.github.com>
2019-11-16 21:57:44 +0700

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,18 +1,17 @@
-function createMatrix(dimensi) {
-    var matrix ="";
-    for(var i=0; i<dimensi*dimensi; i++){
-        matrix+=Math.floor(Math.random() * 9)
-        if(matrix.length === 3 || matrix.length === 7 || matrix.length === 11){
-            matrix+='\n'
+function create_triangle(jumlah_bintang){ 
+    jumlah_bintang = jumlah_bintang + 1;
+    let hasil = ""; 
+    for (let i = 1; i<jumlah_bintang; i++){ 
+        for(let j = 1; j<jumlah_bintang; j++){ 
+            if(i+j >= jumlah_bintang){
+                hasil = hasil + "*";
+            }
+            else{
+                hasil = hasil + " ";
+            }
         }
+        hasil = hasil + "\n";
     }
-
-    var diagonal1= Number(matrix[0]) + Number(matrix[5]) + Number(matrix[10])
-    var diagonal2 = Number(matrix[2]) + Number(matrix[5]) + Number(matrix[8])
-    var result = diagonal1+diagonal2
-
-    console.log(matrix +'\n')
-    console.log('Total: '+ result)
+    console.log(hasil)
 }
-
-createMatrix(3)
+create_triangle(5) 
